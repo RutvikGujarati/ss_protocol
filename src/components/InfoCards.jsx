443accd93fb894174a65bb6d305dbc3209963818
Rutvik Gujarati <mrrutvikgujarati0007@gmail.com>
2024-12-16 18:48:02 +0530

    routing update

--- src/components/InfoCards.jsx
+++ src/components/InfoCards.jsx
@@ -3,6 +3,7 @@ import "../Styles/InfoCards.css";
 import { useDAVToken } from "../Context/DavTokenContext";
 import { useEffect, useState } from "react";
 import { ethers } from "ethers";
+import { useLocation } from "react-router-dom";
 
 const InfoCards = () => {
   const {
@@ -50,91 +51,202 @@ const InfoCards = () => {
     GetCurrentStateReward();
   }, [amount]);
 
-  return (
-    <div className="container mt-4">
-      <div className="row g-4 d-flex align-items-stretch pb-1 border-bottom">
-        <div className="col-md-4 p-0 m-2 cards">
-          <div className="card bg-dark text-light border-light p-3 d-flex w-100 mh200">
-            <div className="carddetaildiv d-flex uppercase">
-              <div className="carddetails">
-                <p className="mb-1 detailText">DAV MINT COST</p>
-                <h5 className="detailAmount">1 DAV MINT COST = 100 000 PLS</h5>
-              </div>
-              <div className="carddetails">
-                <input
-                  type="text"
-                  placeholder="Enter Value"
-                  className="form-control text-center fw-bold cardInput"
-                  value={amount} // Controlled input, using state value
-                  onChange={handleInputChange} // Update state on change
-                />
+  const location = useLocation();
+  const isBurn = location.pathname === "/burn";
+  const isAuction = location.pathname === "/auction";
 
-                <div className="mx-4 text-center">
-                  <h5 className="detailAmount mt-1">
-                    {TotalCost
-                      ? formatNumber(ethers.formatUnits(TotalCost, 18))
-                      : "0"}{" "}
-                    PLS
-                  </h5>
-                  <h5 className="detailAmount mt-1">
-                    {StateReward
-                      ? formatNumber(StateReward) // format STATE
-                      : "0"}{" "}
-                    STATE
-                  </h5>
+  return (
+    <>
+      {isAuction ? (
+        <>
+          <div className="container mt-4">
+            <div className="row g-4 d-flex align-items-stretch pb-1 border-bottom">
+              <div className="col-md-4 p-0 m-2 cards">
+                <div className="card bg-dark text-light border-light p-3 d-flex w-100 mh200">
+                  <div className="carddetaildiv d-flex uppercase">
+                    <div className="carddetails">
+                      <p className="mb-1 detailText">DAV MINT COST</p>
+                      <h5 className="detailAmount">
+                        1 DAV MINT COST = 100 000 PLS
+                      </h5>
+                    </div>
+                    <div className="carddetails">
+                      <input
+                        type="text"
+                        placeholder="Enter Value"
+                        className="form-control text-center fw-bold cardInput"
+                        value={amount} // Controlled input, using state value
+                        onChange={handleInputChange} // Update state on change
+                      />
+
+                      <div className="mx-4 text-center">
+                        <h5 className="detailAmount mt-1">
+                          {TotalCost
+                            ? formatNumber(ethers.formatUnits(TotalCost, 18))
+                            : "0"}{" "}
+                          PLS
+                        </h5>
+                        <h5 className="detailAmount mt-1">
+                          {StateReward
+                            ? formatNumber(StateReward) // format STATE
+                            : "0"}{" "}
+                          STATE
+                        </h5>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="carddetaildiv d-flex">
+                    <div className="carddetails">
+                      <p className="mb-1 detailText">STATE TOKEN REWARD</p>
+                      <h5 className="detailAmount">
+                        {formatNumber(CurrentSReward)}
+                      </h5>
+                    </div>
+                    <div className="carddetails text-center">
+                      <button
+                        onClick={handleMint}
+                        className="btn btn-primary btn-sm w-50 p-1"
+                        disabled={load}
+                      >
+                        {load ? "Minting..." : "Mint"}
+                      </button>
+
+                      <h5 className="detailAmount pt-1">1% SLIPPAGE</h5>
+                    </div>
+                  </div>
+                  <p className="detailfooter p-0 m-0">
+                    {Supply} DAV TOKEN RELEASED
+                  </p>
+                </div>
+              </div>
+              <div className="col-md-4 p-0 m-2 cards">
+                <div className="card bg-dark text-light border-light p-3 d-flex w-100 mh200">
+                  <div className="carddetaildiv uppercase">
+                    <div className="carddetails2">
+                      <p className="mb-1 detailText">Dav holdings</p>
+                      <h5 className="">{davHolds}</h5>
+                    </div>
+                    <div className="carddetails2">
+                      <p className="mb-1 detailText">Dav Rank</p>
+                      <h5 className="">{davPercentage}%</h5>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-4 p-0 m-2 cards">
+                <div className="card bg-dark text-light border-light p-3 d-flex w-100 mh200">
+                  <div className="carddetaildiv uppercase">
+                    <div className="carddetails2">
+                      <p className="mb-1 detailText">State token holdings</p>
+                      <h5 className="">{StateHolds} / $56.90</h5>
+                    </div>
+                    <div className="carddetails2">
+                      <p className="mb-1 detailText">State token price</p>
+                      <h5 className="">$0.000000356</h5>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="carddetaildiv d-flex">
-              <div className="carddetails">
-                <p className="mb-1 detailText">STATE TOKEN REWARD</p>
-                <h5 className="detailAmount">{formatNumber(CurrentSReward)}</h5>
-              </div>
-              <div className="carddetails text-center">
-                <button
-                  onClick={handleMint}
-                  className="btn btn-primary btn-sm w-50 p-1"
-                  disabled={load}
-                >
-                  {load ? "Minting..." : "Mint"}
-                </button>
+          </div>
+        </>
+      ) : isBurn ? (
+        <>
+          <div className="container mt-4">
+            <div className="row g-4 d-flex align-items-stretch pb-1 border-bottom">
+              <div className="col-md-4 p-0 m-2 cards">
+                <div className="card bg-dark text-light border-light p-3 d-flex w-100 mh200">
+                  <div className="carddetaildiv d-flex uppercase">
+                    <div className="carddetails">
+                      <p className="mb-1 detailText">DAV MINT COST</p>
+                      <h5 className="detailAmount">
+                        1 DAV MINT COST = 100 000 PLS
+                      </h5>
+                    </div>
+                    <div className="carddetails">
+                      <input
+                        type="text"
+                        placeholder="Enter Value"
+                        className="form-control text-center fw-bold cardInput"
+                        value={amount} // Controlled input, using state value
+                        onChange={handleInputChange} // Update state on change
+                      />
 
-                <h5 className="detailAmount pt-1">1% SLIPPAGE</h5>
+                      <div className="mx-4 text-center">
+                        <h5 className="detailAmount mt-1">
+                          {TotalCost
+                            ? formatNumber(ethers.formatUnits(TotalCost, 18))
+                            : "0"}{" "}
+                          PLS
+                        </h5>
+                        <h5 className="detailAmount mt-1">
+                          {StateReward
+                            ? formatNumber(StateReward) // format STATE
+                            : "0"}{" "}
+                          STATE
+                        </h5>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="carddetaildiv d-flex">
+                    <div className="carddetails">
+                      <p className="mb-1 detailText">STATE TOKEN REWARD</p>
+                      <h5 className="detailAmount">
+                        {formatNumber(CurrentSReward)}
+                      </h5>
+                    </div>
+                    <div className="carddetails text-center">
+                      <button
+                        onClick={handleMint}
+                        className="btn btn-primary btn-sm w-50 p-1"
+                        disabled={load}
+                      >
+                        {load ? "Minting..." : "Mint"}
+                      </button>
+
+                      <h5 className="detailAmount pt-1">1% SLIPPAGE</h5>
+                    </div>
+                  </div>
+                  <p className="detailfooter p-0 m-0">
+                    {Supply} DAV TOKEN RELEASED
+                  </p>
+                </div>
+              </div>
+              <div className="col-md-4 p-0 m-2 cards">
+                <div className="card bg-dark text-light border-light p-3 d-flex w-100 mh200">
+                  <div className="carddetaildiv uppercase">
+                    <div className="carddetails2">
+                      <p className="mb-1 detailText">Dav holdings</p>
+                      <h5 className="">{davHolds}</h5>
+                    </div>
+                    <div className="carddetails2">
+                      <p className="mb-1 detailText">Dav Rank</p>
+                      <h5 className="">{davPercentage}%</h5>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-4 p-0 m-2 cards">
+                <div className="card bg-dark text-light border-light p-3 d-flex w-100 mh200">
+                  <div className="carddetaildiv uppercase">
+                    <div className="carddetails2">
+                      <p className="mb-1 detailText">State token holdings</p>
+                      <h5 className="">{StateHolds} / $56.90</h5>
+                    </div>
+                    <div className="carddetails2">
+                      <p className="mb-1 detailText">State token price</p>
+                      <h5 className="">$0.000000356</h5>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-            <p className="detailfooter p-0 m-0">{Supply} DAV TOKEN RELEASED</p>
           </div>
-        </div>
-        <div className="col-md-4 p-0 m-2 cards">
-          <div className="card bg-dark text-light border-light p-3 d-flex w-100 mh200">
-            <div className="carddetaildiv uppercase">
-              <div className="carddetails2">
-                <p className="mb-1 detailText">Dav holdings</p>
-                <h5 className="">{davHolds}</h5>
-              </div>
-              <div className="carddetails2">
-                <p className="mb-1 detailText">Dav Rank</p>
-                <h5 className="">{davPercentage}%</h5>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-4 p-0 m-2 cards">
-          <div className="card bg-dark text-light border-light p-3 d-flex w-100 mh200">
-            <div className="carddetaildiv uppercase">
-              <div className="carddetails2">
-                <p className="mb-1 detailText">State token holdings</p>
-                <h5 className="">{StateHolds} / $56.90</h5>
-              </div>
-              <div className="carddetails2">
-                <p className="mb-1 detailText">State token price</p>
-                <h5 className="">$0.000000356</h5>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+        </>
+      ) : (
+        <></>
+      )}
+    </>
   );
 };
 
