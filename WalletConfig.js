937c53a5b1b74ff741da00cf0f5fe255f2caa8f8
Rutvik Gujarati <mrrutvikgujarati0007@gmail.com>
2025-03-09 09:29:00 +0530

    removed bsC

--- WalletConfig.js
+++ WalletConfig.js
@@ -1,9 +1,9 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { pulsechain,bsc } from 'viem/chains';
+import { pulsechain } from 'viem/chains';
 
 export const config = getDefaultConfig({
 	appName: 'ss-protocol',
 	projectId: '204f006744ca21d941871530c5aea514',
-	chains: [pulsechain,bsc],
+	chains: [pulsechain],
 	ssr: true,
 });
