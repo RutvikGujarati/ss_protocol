57738d15207c0ab63988b97326d7a1374138cdaa
Rutvik Gujarati <mrrutvikgujarati0007@gmail.com>
2025-05-16 15:21:47 +0530

    state token added comments

--- state-contracts/src/StateToken.sol
+++ state-contracts/src/StateToken.sol
@@ -4,20 +4,35 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract STATE_Token_V2_1_Ratio_Swapping is ERC20 ,Ownable(msg.sender){
-    uint256 public constant MAX_SUPPLY = 1000000000000000 ether; // 1 QUADRILLION
+/// @title STATE Token V2.1 with Ratio-Based Initial Supply Allocation
+/// @author
+/// @notice ERC20 token with 5/95 minting distribution between two addresses at deployment
+/// @dev Uses OpenZeppelin ERC20 and Ownable contracts
+contract StateTokenV2_1 is ERC20, Ownable(msg.sender) {
+    /// @notice Maximum total supply of the token (1 quadrillion tokens, 18 decimals)
+    uint256 public constant MAX_TOTAL_SUPPLY = 1_000_000_000_000_000 ether;
 
+    /// @param tokenName The name of the token
+    /// @param tokenSymbol The symbol of the token
+    /// @param recipientFivePercent Address receiving 5% of total supply
+    /// @param recipientNinetyFivePercent Address receiving 95% of total supply
     constructor(
-        string memory name,
-        string memory symbol,
-        address _five,
-        address _swap
-    ) ERC20(name, symbol) {
-        require(_five != address(0) && _swap != address(0), "Invalid address");
+        string memory tokenName,
+        string memory tokenSymbol,
+        address recipientFivePercent,
+        address recipientNinetyFivePercent
+    ) ERC20(tokenName, tokenSymbol) {
+        require(recipientFivePercent != address(0), "Invalid 5% address");
+        require(
+            recipientNinetyFivePercent != address(0),
+            "Invalid 95% address"
+        );
 
-        uint256 fivePercent = (MAX_SUPPLY * 5) / 100;
-        uint256 ninetyFivePercent = MAX_SUPPLY - fivePercent;
-        _mint(_five, fivePercent);
-        _mint(_swap, ninetyFivePercent);
+        uint256 fivePercentAllocation = (MAX_TOTAL_SUPPLY * 5) / 100;
+        uint256 ninetyFivePercentAllocation = MAX_TOTAL_SUPPLY -
+            fivePercentAllocation;
+
+        _mint(recipientFivePercent, fivePercentAllocation);
+        _mint(recipientNinetyFivePercent, ninetyFivePercentAllocation);
     }
 }
