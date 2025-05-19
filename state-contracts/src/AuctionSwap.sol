f597ec4f516007bd82c701a16463b267f1cecb4c
Rutvik Gujarati <mrrutvikgujarati0007@gmail.com>
2025-05-19 16:34:34 +0530

    testnet parameters

--- state-contracts/src/AuctionSwap.sol
+++ state-contracts/src/AuctionSwap.sol
@@ -37,9 +37,9 @@ contract Ratio_Swapping_Auctions_V2_1 is Ownable(msg.sender), ReentrancyGuard {
     }
 
     //For Airdrop
-    uint256 public constant AUCTION_INTERVAL = 4 hours;
-    uint256 public constant AUCTION_DURATION = 2 hours;
-    uint256 public constant REVERSE_DURATION = 2 hours;
+    uint256 public constant AUCTION_INTERVAL = 1 hours;
+    uint256 public constant AUCTION_DURATION = 1 hours;
+    uint256 public constant REVERSE_DURATION = 1 hours;
     uint256 public constant MAX_AUCTIONS = 20;
     uint256 public constant OWNER_REWARD_AMOUNT = 2500000 * 1e18;
     uint256 public constant CLAIM_INTERVAL = 4 hours;
