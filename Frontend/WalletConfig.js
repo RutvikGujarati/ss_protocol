ac61ebe406b1271e788cbdedd48754e814ac9cde
Rutvik Gujarati <mrrutvikgujarati0007@gmail.com>
2025-06-19 13:59:11 +0530

    polygon chain added

--- Frontend/WalletConfig.js
+++ Frontend/WalletConfig.js
@@ -1,10 +1,10 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { pulsechain, pulsechainV4 } from 'viem/chains';
+import { polygon, pulsechain, pulsechainV4 } from 'viem/chains';
 
 export const config = getDefaultConfig({
 	appName: 'ss-protocol',
 	projectId: '204f006744ca21d941871530c5aea514',
-	chains: [pulsechain],
+	chains: [pulsechain,polygon],
 	ssr: true,
 	autoConnect: true,
 });
