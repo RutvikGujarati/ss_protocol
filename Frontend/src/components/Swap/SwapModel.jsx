3fdf4afafd98d0570750ded827bc16985884ccfe
RutvikGujarati <mrrutvikgujarati0007@gmail.com>
2025-07-30 16:15:25 +0530

    text size fix of swap txt

--- Frontend/src/components/Swap/SwapModel.jsx
+++ Frontend/src/components/Swap/SwapModel.jsx
@@ -533,7 +533,7 @@ const SwapComponent = () => {
                       className="btn btn-success rounded-pill py-2"
                       onClick={handleApprove}
                       disabled={isApproving || isSwapping}
-                      style={{ width: "170px", padding: "10px 20px", fontWeight: 400, height: "40px", textAlign: "center" }}
+                      style={{ width: "170px",fontSize:"16px", padding: "10px 20px", fontWeight: 400, height: "40px", textAlign: "center" }}
                     >
                       {isApproving ? (
                         <>
@@ -552,7 +552,7 @@ const SwapComponent = () => {
                       className="btn btn-primary rounded-pill py-2"
                       onClick={handleSwap}
                       disabled={!quoteData || isSwapping || insufficientBalance}
-                      style={{ width: "170px", padding: "10px 20px", fontWeight: 400, height: "40px", textAlign: "center" }}
+                      style={{ width: "170px",fontSize:"16px", padding: "10px 20px", fontWeight: 400, height: "40px", textAlign: "center" }}
                     >
                       {isSwapping ? (
                         <>
