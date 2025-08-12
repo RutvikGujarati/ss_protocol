5b94f4a75df6ec8ab0af743f0690e9bb98536d1e
RutvikGujarati <mrrutvikgujarati0007@gmail.com>
2025-08-12 10:57:57 +0530

    removed amoy

--- Frontend/WalletConfig.js
+++ Frontend/WalletConfig.js
@@ -1,10 +1,10 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { polygon, pulsechain, sonic, mainnet,polygonAmoy } from 'viem/chains';
+import { polygon, pulsechain, sonic, mainnet } from 'viem/chains';
 
 export const config = getDefaultConfig({
 	appName: 'ss-protocol',
 	projectId: '204f006744ca21d941871530c5aea514',
-	chains: [pulsechain, polygonAmoy,polygon, sonic, mainnet],
+	chains: [pulsechain, polygon, sonic, mainnet],
 	ssr: true,
 	autoConnect: true,
 });
@@ -14,6 +14,5 @@ export const chainCurrencyMap = {
 	[polygon.id]: polygon.nativeCurrency.symbol,
 	[sonic.id]: sonic.nativeCurrency.symbol,
 	[pulsechain.id]: pulsechain.nativeCurrency.symbol,
-	[polygonAmoy.id]: polygonAmoy.nativeCurrency.symbol,
 	[mainnet.id]: mainnet.nativeCurrency.symbol,
 };
