8b26ba9ca787bba7e3b672710519dd184d961d86
RutvikGujarati <mrrutvikgujarati0007@gmail.com>
2025-08-25 14:15:09 +0530

    devide with 100

--- Frontend/src/components/Cards/AuctionSection.jsx
+++ Frontend/src/components/Cards/AuctionSection.jsx
@@ -344,7 +344,7 @@ const AuctionSection = () => {
                                                         ({DaipriceChange} %)
                                                     </span>{" "}
                                                     {formatWithCommas(
-                                                        Math.max(calculateTotalSum() * DaipriceChange, 0) * 100
+                                                        Math.max(calculateTotalSum() * DaipriceChange, 0) / 100
                                                     )}{" "}
 
                                                     PLS
