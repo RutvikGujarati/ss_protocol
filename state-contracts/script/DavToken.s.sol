e732dfc570603b7fafc7b5df9dee1be71055eb64
RutvikGujarati <mrrutvikgujarati0007@gmail.com>
2025-08-25 20:50:11 +0530

    Mainnet Deployment V.3 PLS

--- state-contracts/script/DavToken.s.sol
+++ state-contracts/script/DavToken.s.sol
@@ -10,7 +10,7 @@ contract ScriptDAV is Script {
         //NOTE: add correct wallets for Gov and Dev
         address liquidity = 0x98b0379474Cf84Ab257bEe0b73dceb11051223A5;
         address DAVWallet = 0x1262f7De33bA34C9373C20199fAb73CaCE13B5E9;
-		address state = 0xCF93C29db1D6A8dA724536E68ca413883f4Fd9a2;
+		address state = 0x6E76680b7AB936D1613b20f1a7589EaB3A3b862A;
         address Governanace = 0x98b0379474Cf84Ab257bEe0b73dceb11051223A5;
 
         DAV_V3 dav = new DAV_V3(
--- state-contracts/script/TokenDeploy.s.sol
+++ state-contracts/script/TokenDeploy.s.sol
@@ -8,7 +8,7 @@ contract DeployState is Script {
     function run() external {
         vm.startBroadcast();
         address Five = 0x98b0379474Cf84Ab257bEe0b73dceb11051223A5;
-        address Swap = 0x97d7b4d458C2d4CbFDd2Ee4585090FDD92249802;
+        address Swap = 0xD1C3485Cc4cc39F21Fe7779998F08eC141f7A7dA;
         //NOTE: Mainnet token name is pSTATE
         STATE_V3 state = new STATE_V3("pSTATE", "pSTATE", Five, Swap);
 
